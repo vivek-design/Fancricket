298d71cfede4ded3d66f6420362f54a9c905b2a9
vivek chouhan <79709538+vivek-design@users.noreply.github.com>
2024-02-27 00:11:23 +0530

    something added

--- lib/main.dart
+++ lib/main.dart
@@ -1,86 +1,68 @@
-import 'dart:convert';
-import 'dart:typed_data';
-
-import 'package:fancrick/Admin/adminfront.dart';
 import 'package:fancrick/Features/Auth/auth.dart';
-import 'package:fancrick/Features/Register/rergister.dart';
-import 'package:fancrick/Features/Register/verifyotp.dart';
 import 'package:fancrick/Provider/userprovider.dart';
-import 'package:fancrick/User/userfront.dart';
-import 'package:fancrick/Utilities/constants.dart';
 import 'package:fancrick/welcome.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
-  runApp(MultiProvider(providers: [
-    ChangeNotifierProvider(
-      create: (context) => UserProvider(),
-    )
-  ], child: const MyApp()));
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => UserProvider(),
+        )
+      ],
+      child: MyApp(),
+    ),
+  );
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    print(Provider.of<UserProvider>(context).user.name.toString());
     return MaterialApp(
-      title: "Fancricket ",
+      title: "Fancricket",
       debugShowCheckedModeBanner: false,
-      home: mytempapp(),
+      home: MyTempApp(),
     );
   }
 }
 
-class mytempapp extends StatefulWidget {
-  const mytempapp({super.key});
-
+class MyTempApp extends StatefulWidget {
   @override
-  State<mytempapp> createState() => _mytempappState();
+  _MyTempAppState createState() => _MyTempAppState();
 }
 
-class _mytempappState extends State<mytempapp> {
+class _MyTempAppState extends State<MyTempApp> {
   final AuthService authService = AuthService();
-  Future<bool> fun() async {
-    AuthService auth = new AuthService();
-    SharedPreferences pref = await SharedPreferences.getInstance();
-    String email = await pref.get('email').toString();
-    String password = await pref.get('password').toString();
-
-    if (email == password) {
-      Navigator.of(context).push(MaterialPageRoute(builder: (context) {
-        return gettingstarted();
-      }));
-
-      // calimage();
-    } else {
-      print("In");
-      auth.signInUser(context: context, email: email, password: password);
-      
-    }
-    return true;
-  }
 
   @override
   void initState() {
     super.initState();
-    fun();
+    checkLoginStatus();
+  }
+
+  Future<void> checkLoginStatus() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    final String? email = prefs.getString('email');
+    final String? password = prefs.getString('password');
+
+    if (email == password) {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (context) => gettingstarted()),
+      );
+    } else {
+      print("Logged out");
+      // Handle logout or show login screen
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Center(child: CircularProgressIndicator()),
+      body: Center(
+        child: CircularProgressIndicator(),
       ),
     );
   }
